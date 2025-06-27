199730126ffd7e39ae270e857f71f5ccb91be5c4
pavelfedotov <pahaa97@gmail.com>
2025-06-27 18:14:01 +0200

    fixed reloading nginx

--- deploy.php
+++ deploy.php
@@ -194,7 +194,7 @@ class SchemaDeployer
         $output = [];
         $returnCode = 0;
 
-        exec('systemctl restart nginx 2>&1', $output, $returnCode);
+        exec('v-restart-web 2>&1', $output, $returnCode);
 
         if ($returnCode === 0) {
             Logger::log("Nginx reloaded successfully");
@@ -327,6 +327,7 @@ class SchemaDeployer
         StateManager::save($previousState);
 
         if ($needsNginxReload) {
+            sleep(3);
             self::reloadNginx();
         }
 
